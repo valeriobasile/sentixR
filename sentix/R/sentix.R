c1f17f556d85a888422b7fdc5538b110e3939785
Valerio Basile <valeriobasile@gmail.com>
2019-03-07 17:15:29 +0100

    first upload

--- sentix/R/sentix.R
+++ sentix/R/sentix.R
@@ -11,6 +11,8 @@
 sentix <- read.delim("sentix_lemma2.0.tsv", header=F, quote="", colClasses=c("character", "numeric"))
 names(sentix) <- c("lemma", "value")
 
+# load the UDpipe model for italian
+udmodel_italian <- udpipe_load_model(file = "italian-isdt-ud-2.3-181115.udpipe")
 
 # function to tokenize and lemmatize the input text using UDpipe
 get_lemmas <- function(txt){
